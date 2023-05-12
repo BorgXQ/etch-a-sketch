73523a7d235ef40df69f8c5d552bbfdbd4c78db1
BorgXQ <jonathan.widarsa@gmail.com>
2023-05-12 23:55:24 +0800

    Add new createGrid function

--- myscript.js
+++ myscript.js
@@ -1,18 +1,17 @@
-function createGrid() {
-    const n = 16
-    for (let i=0; i<n; i++) {
-        let myRow = document.createElement("tr");
-        myRow.id = "row" + i;
-        tbl.appendChild(myRow);
-
-        const useRow = document.getElementById("row" + i);
-        for (let j=0; j<n; j++) {
-            let myColumn = document.createElement("td");
-            myColumn.id = "column" + j;
-            useRow.appendChild(myColumn);
-        }
+function createGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+  
+    for (let i = 0; i < size * size; i++) {
+      const gridElement = document.createElement('div')
+      gridElement.classList.add('grid-element')
+      /*gridElement.addEventListener('mouseover', changeColor)
+      gridElement.addEventListener('mousedown', changeColor)*/
+      grid.appendChild(gridElement)
     }
-}
+  }
 
 const tbl = document.getElementById("tbl");
-createGrid();
+createGrid(16);
+
+
