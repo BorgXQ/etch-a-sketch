0540c44b862446f53cb84f261ee9d3c1d0d8b6dc
BorgXQ <jonathan.widarsa@gmail.com>
2023-05-13 00:38:21 +0800

    Add connection between slider and grid

--- myscript.js
+++ myscript.js
@@ -1,27 +1,52 @@
+const DEFAULT_SIZE = 16;
+let currentSize = DEFAULT_SIZE;
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
+
 function createGrid(size) {
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   
     for (let i = 0; i < size * size; i++) {
-      const gridElement = document.createElement('div')
-      gridElement.classList.add('grid-element')
-      /*gridElement.addEventListener('mouseover', changeColor)
-      gridElement.addEventListener('mousedown', changeColor)*/
-      grid.appendChild(gridElement)
+      const gridElement = document.createElement('div');
+      gridElement.classList.add('grid-element');
+      grid.appendChild(gridElement);
     }
   }
 
-function trackSlider() {
-    rangeName.innerHTML = `${this.value} x ${this.value}`;
+function trackSlider(value) {
+    rangeName.innerHTML = `${value} x ${value}`;
 }
 
-const tbl = document.getElementById("tbl");
-createGrid(16);
+function changeSize(value) {
+    setCurrentSize(value);
+    trackSlider(value);
+    reloadGrid();
+}
 
-let slide = document.getElementById("slide");
-let rangeName = document.getElementById("rangeName");
-rangeName.innerHTML = `${slide.value} x ${slide.value}`;
+function reloadGrid() {
+    grid.innerHTML = "";
+    createGrid(currentSize);
+}
 
-slide.oninput = function() {
-    rangeName.innerHTML = `${this.value} x ${this.value}`;
-};
+const rangeName = document.getElementById("rangeName");
+const slide = document.getElementById("slide");
+const grid = document.getElementById("grid");
+
+slide.onmousemove = (e) => trackSlider(e.target.value)
+slide.onchange = (e) => changeSize(e.target.value)
+
+window.onload = () => {
+    createGrid(DEFAULT_SIZE);
+}
+
+// rangeName.innerHTML = `${slide.value} x ${slide.value}`;
+
+// slide.oninput = function() {
+//     rangeName.innerHTML = `${this.value} x ${this.value}`;
+// };
+
+// const tbl = document.getElementById("tbl");
+// createGrid(slide.value);
